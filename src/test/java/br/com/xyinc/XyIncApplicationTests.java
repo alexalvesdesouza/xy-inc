1c6018c2f0139a21a014df64dd28e284e421fabc
Alex Alves de Souza <alvesdesouzaalex@gmail.com>
2017-09-13 02:02:23 -0300

    Criação da classe de teste automatizado

--- src/test/java/br/com/xyinc/XyIncApplicationTests.java
+++ src/test/java/br/com/xyinc/XyIncApplicationTests.java
@@ -1,16 +1,67 @@
 package br.com.xyinc;
 
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class XyIncApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+	}
+
 	@Test
-	public void contextLoads() {
+	public void nenhumPontoInteresseEncontrado() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void validaCoordenadasNulas() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void validaCoordenadasNegativas() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void validaNomePontoInteresseNulo() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void inserindoPontosInteresse() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void localizandoTodosPontosInteresse() {
+		// TODO Ajustar nome do teste e ordem de execução
+	}
+
+	@Test
+	public void localizandoPontosInteressePorCoordenadasEDistancia() {
+		// TODO Ajustar nome do teste e ordem de execução
 	}
 
 }
